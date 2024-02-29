84e47ad87526cce768a4c134eecbd6078f457673
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-02-29 15:22:51 +0300

    end of the lesson 16

---
+++ Blogy.DataAccessLayer/Migrations/20240229113737_create_relationships.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blogy.DataAccessLayer.Migrations
+{
+    public partial class create_relationships : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ArticleId",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Articles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WriterId",
+                table: "Articles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_ArticleId",
+                table: "Comments",
+                column: "ArticleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_CategoryId",
+                table: "Articles",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_WriterId",
+                table: "Articles",
+                column: "WriterId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articles_Categories_CategoryId",
+                table: "Articles",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articles_Writers_WriterId",
+                table: "Articles",
+                column: "WriterId",
+                principalTable: "Writers",
+                principalColumn: "WriterId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Articles_ArticleId",
+                table: "Comments",
+                column: "ArticleId",
+                principalTable: "Articles",
+                principalColumn: "ArticleId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articles_Categories_CategoryId",
+                table: "Articles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articles_Writers_WriterId",
+                table: "Articles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Articles_ArticleId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_ArticleId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_CategoryId",
+                table: "Articles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_WriterId",
+                table: "Articles");
+
+            migrationBuilder.DropColumn(
+                name: "ArticleId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Articles");
+
+            migrationBuilder.DropColumn(
+                name: "WriterId",
+                table: "Articles");
+        }
+    }
+}
