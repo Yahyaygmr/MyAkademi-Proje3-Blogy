41985fbda5a4115f56916de04d39dc322265486a
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-03-14 15:59:25 +0300

    end of lesson 16

--- Blogy.BusinessLayer/Abstaract/ICategoryService.cs
+++ Blogy.BusinessLayer/Abstaract/ICategoryService.cs
@@ -9,5 +9,6 @@ namespace Blogy.BusinessLayer.Abstaract
 {
     public interface ICategoryService : IGenericService<Category>
     {
+        int TGetCategoryCount();
     }
 }
--- Blogy.DataAccessLayer/EntityFramework/EfCategoryDal.cs
+++ Blogy.DataAccessLayer/EntityFramework/EfCategoryDal.cs
@@ -12,8 +12,15 @@ namespace Blogy.DataAccessLayer.EntityFramework
 {
     public class EfCategoryDal : GenericRepository<Category>, ICategoryDal
     {
+        private readonly BlogyContext _context;
         public EfCategoryDal(BlogyContext context) : base(context)
         {
+            _context = context;
+        }
+
+        public int GetCategoryCount()
+        {
+            return _context.Categories.Count();
         }
     }
 }
