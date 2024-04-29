edcc11f8b8a068bfacc9395db85ccaf2ade92647
Yahyaygmr <yahyaygmr@gmail.com>
2024-04-29 12:04:15 +0300

    created weather api

--- Blogy.WebUI/Areas/Writer/ViewComponents/LayoutViewComponents/_LayoutNavBarComponentPartial.cs
+++ Blogy.WebUI/Areas/Writer/ViewComponents/LayoutViewComponents/_LayoutNavBarComponentPartial.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blogy.BusinessLayer.Abstaract;
+using Blogy.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blogy.WebUI.Areas.Writer.ViewComponents.LayoutViewComponents
 {
     public class _LayoutNavBarComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly UserManager<AppUser> _userManager;
+
+        public _LayoutNavBarComponentPartial(UserManager<AppUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            ViewBag.userName = user.Name + " " + user.Surname.ToUpper();
+            ViewBag.image = user.ImageUrl;
             return View();
         }
     }
--- Blogy.WebUI/Areas/Writer/ViewComponents/LayoutViewComponents/_LayoutSideBarComponentPartial.cs
+++ Blogy.WebUI/Areas/Writer/ViewComponents/LayoutViewComponents/_LayoutSideBarComponentPartial.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blogy.BusinessLayer.Abstaract;
+using Blogy.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Text;
 
 namespace Blogy.WebUI.Areas.Writer.ViewComponents.LayoutViewComponents
 {
     public class _LayoutSideBarComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IArticleService _articleService;
+
+        public _LayoutSideBarComponentPartial(UserManager<AppUser> userManager, IArticleService articleService)
         {
+            _userManager = userManager;
+            _articleService = articleService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var blogCount = _articleService.TGetArticlesByArticleByWriter(user.Id).Count();
+
+            ViewBag.userName = user.Name +" "+ user.Surname.ToUpper();
+            ViewBag.blogCount = blogCount;
+            ViewBag.image = user.ImageUrl;
+
             return View();
         }
     }
