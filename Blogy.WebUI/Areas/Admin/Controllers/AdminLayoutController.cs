8bce86a115ebf41bdf1a847d5093b24f4b18d985
Yahyaygmr <yahyaygmr@gmail.com>
2024-04-29 17:20:18 +0300

    adjusments to admin theme

--- Blogy.WebUI/Areas/Admin/Controllers/AdminLayoutController.cs
+++ Blogy.WebUI/Areas/Admin/Controllers/AdminLayoutController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 namespace Blogy.WebUI.Areas.Admin.Controllers
 {
     [Authorize(Roles = "Admin")]
+    [Area("Admin")]
+    [Route("Admin/{controller}/{action}/{id?}")]
     public class AdminLayoutController : Controller
     {
         public IActionResult Index()
---
+++ Blogy.WebUI/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.Areas.Admin.Controllers
+{
+    [AllowAnonymous]
+    [Authorize(Roles = "Admin")]
+    [Area("Admin")]
+    [Route("Admin/{controller}/{action}/{id?}")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
